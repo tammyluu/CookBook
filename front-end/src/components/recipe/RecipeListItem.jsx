59dd3d50c422836b86dd5594dcfaa4333c93e71f
Pauline <pauline@MacBook-Air-de-Tom.local>
2023-11-02 21:50:54 +0100

    recipeListItem ok

--- front-end/src/components/recipe/RecipeListItem.jsx
+++ front-end/src/components/recipe/RecipeListItem.jsx
@@ -3,25 +3,24 @@ import classes from "./RecipeListItem.module.css"
 const RecipeListItem = () => {
     return ( 
         <>
-        <div>
             <div className={classes.card}>
                 <img className={classes.img} src="https://images.unsplash.com/photo-1602253057119-44d745d9b860?auto=format&fit=crop&q=80&w=1926&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
-                <div className={classes.cardBody}>
-                    <h4 className={classes.title}>Recette 1</h4>
-                    <div className={classes.txt}>
-                        <span>Temps préparation</span>
-                        <span>Difficulté</span>
-                        <span>Budget</span>
-                    <p>phrase d'accroche</p>
+                <div className="col ms-3 mb-3">
+                    <h4 className={classes.title}>Poulet sauté aux légumes</h4>
+                    <div className="mb-3">
+                        <span className="me-3"><i className="bi bi-stopwatch me-1"></i>15min</span>
+                        <span className="me-3"><i className="bi bi-star-fill"></i><i className="bi bi-star-fill"></i><i className="bi bi-star-fill me-1"></i>Difficulté</span>
+                        <span className="me-3"><i className="bi bi-currency-euro me-1"></i>Budget</span>
+                    <p className="mt-2">Phrase d'accroche</p>
                     </div>
                         <button className={classes.details}>Détails</button>
-                        <div className={classes.buttons}> 
+                        <div className="text-end me-2"> 
                             <button className={classes.edit}>Modifier</button>
+                        <div className={classes.space}> </div>
                             <button className={classes.delete}>Supprimer</button>
-                        </div>   
+                        </div>
                 </div>
             </div>
-        </div>
         </>
      );
 }
